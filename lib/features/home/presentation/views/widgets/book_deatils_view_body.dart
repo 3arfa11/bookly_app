faa437c66d3c3f17b0103d628f518e499b9f2c7d
Mohamed Sami <139673490+3arfa11@users.noreply.github.com>
2025-05-07 15:21:47 +0300

    Book Details View UI , Search View UI Done

--- lib/features/home/presentation/views/widgets/book_deatils_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_deatils_view_body.dart
@@ -1,10 +1,6 @@
-import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_section.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_image_item.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/features/home/presentation/views/widgets/book_suggestion_list_view.dart';
+
 import 'package:bookly_app/features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/suggested_books_section.dart';
 import 'package:flutter/material.dart';
